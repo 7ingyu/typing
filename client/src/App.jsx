58aa971adf8fc24032e240c6f15878f198b6372b
Ting-Yu Chen <tingyuchen@Ting-Yus-MacBook-Pro.local>
2021-05-18 12:16:37 -0700

    Restart button now functional

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,17 +5,23 @@ const App = () => {
   const [timeDisplay, setTimeDisplay] = useState(timer);
   const [btnText, setBtnText] = useState('Start');
   const [timerStarted, setTimerStarted] = useState(false);
+  const [typedText, setTypedText] = useState('');
+  var countdown;
 
   const handleTyping = (event) => {
+
+    setTypedText(event.target.value);
+
     if (timer > 0 && timerStarted === false) {
       setTimerStarted(true);
       let timeLeft = timer
-      var countdown = setInterval(() => {
+      countdown = setInterval(() => {
         console.log(timeLeft);
-        if (timeLeft >= 0) {
-          setTimeDisplay(timeLeft--);
+        if (timeLeft > 0) {
+          setTimeDisplay(--timeLeft);
         } else {
-          event.target.setAttribute("disabled", true);
+          event.target.disabled = true;
+          clearInterval(countdown);
         }
       }, 1000);
     }
@@ -23,7 +29,11 @@ const App = () => {
 
   const handleReset = () => {
     setTimeDisplay(timer);
-    clearInterval(countdown);
+    setTimerStarted(false);
+    let textbox = document.getElementById('typingarea');
+    textbox.value = '';
+    textbox.disabled = false;
+    textbox.focus();
   }
 
   // Refactor to 00:00 time remaining if possible
@@ -31,13 +41,14 @@ const App = () => {
     <div>
       <div role="timer">Seconds Remaining: {timeDisplay}</div>
       <textarea
+        id="typingarea"
         rows="10"
         cols="100"
         autoFocus
         placeholder="Type here!"
         spellCheck="false"
         onChange={ event => handleTyping(event) } />
-      {timeDisplay === 0 ? <button onClick={handleReset}>Restart</button>: null}
+      {timeDisplay <= 0 ? <button autoFocus onClick={handleReset}>Restart</button>: null}
     </div>
   );
 };
